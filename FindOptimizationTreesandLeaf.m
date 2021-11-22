291a19eb8780d45a927217fb9e501731abc3aab8
YZHLhappy <63792824+YZHLhappy@users.noreply.github.com>
2021-11-22 18:09:54 +0100

    Update FindOptimizationTreesandLeaf.m
    Edit comment

--- FindOptimizationTreesandLeaf.m
+++ FindOptimizationTreesandLeaf.m
@@ -1,23 +1,22 @@
-function [] = FindOptimizationTreesandLeaf(Num,Type,Input_train,Output_train,MaxNumTree)
-%% 随机森林最有叶子数和树的参数确认
-% Number of Leaves and Trees Optimization
-% 
-% Overview:This function is used to 计算最优叶子数和树的数量。
+% Overview:This function is used to calculate the optimal number of leaves and tree parameters in a random forest.
 % Created time: 2021.11.22
-% Author: YZHL
-% 参考：https://zhuanlan.zhihu.com/p/367491476
+% Author: YZHLhappy
+% Reference：https://zhuanlan.zhihu.com/p/367491476，Most of the programs refer to this article, I just modified it into a convenient function, thank the author of the article.
 % Modify record: 2021.11.22 Create this function
-% nLeaf = Output_1;
-% nTree = Output_2;
-% Num = Input_1;    % 循环的次数
-% (作废） RFLeaf = Input_2; % 默认为1，此时：RFLeaf=[5,10,20,50,100,200,500];
-%                   % 如果需要修改，需要使用者自行修改函数plot部分
+
+function [] = FindOptimizationTreesandLeaf(Num,Type,Input_train,Output_train,MaxNumTree)
+% nLeaf = Output_1; % Not added yet
+% nTree = Output_2; % Not added yet
+
+% Num = Input_1;    % Number of cycles
+% (未添加） RFLeaf = Input_2;   % The default is 1, at this time: RFLeaf=[5,10,20,50,100,200,500];
+%                               % If you need to modify it, you need to modify the plot part of the function by yourself.
 % Type = Input_2; Type=1,classification; 
 %                 Type=2,regression;
-% Input_train = Input_3; % 训练数据的自变量部分
-% Output_train = Input_4; % 训练数据的标签部分（classification）
-%                         % 训练数据的回归值部分（regression）
-% MaxNumTree = Input_5;   % 期望最大的树的数量（例如2000，此函数将测试1到2000棵树的对应精度
+% Input_train = Input_3;  % The independent variable part of the training data
+% Output_train = Input_4; % The label part of the training data (classification)
+%                         % Regression part of the training data (regression)
+% MaxNumTree = Input_5;   % Set the maximum number of trees (for example, 2000, this function will test the corresponding error value of 1 to 2000 trees)
 for RFOptimizationNum=1:Num
 
 RFLeaf=[5,10,20,50,100,200,500];
